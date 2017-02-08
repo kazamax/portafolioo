3892a408547f8c1dde2fe50f175e2257fbce52cc
kazamax <robertsilkro@hotmail.com>
2017-02-08 09:55:34 -0400

    index
    index

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		
-		<title>Diseño de paginas web profesionales, para tu negocio, empresa u oficina.</title>
+		<title>Diseño de paginas web para tu negocio, empresa u oficina.</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<meta name="keywords" content="">
-		<meta name="description" content="">
-		<meta name="author" content="robertodevel.com">
+		<meta name="description" content="Freelance, Diseño y programación de paginas web, tiendas online, sitios web profesionales para ti, tu negocio, o empresa freelance Roberto Figuera Venezuela">
+		<meta name="author" content="http://roberto-figuera.16mb.com/">
 		<meta name="owner" content="Roberto Jose Figuera Espinoza">
 		<meta name="robots" content="index, follow">
 		
@@ -15,7 +15,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Farsan" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-		<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,all"/>
+		<link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,all"/>
 		<link href="css/redes.css" type="text/css" rel="stylesheet" media="screen,all"/>
 		<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,all"/>
 		<link href="css/style2.css" type="text/css" rel="stylesheet" media="screen,all"/>
@@ -27,6 +27,16 @@
 		</style>
 	</head>
 	<body>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-91652309-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 
 		<section class="content_general">
 
@@ -242,7 +252,7 @@
 								</div>
 								<div class="back white">
 									<p class="center-align titulosCubo">Responsabilidad y puntualidad</p>
-									<p class="left-align parrafosServiciosCubo">Es la forma como me tomo cada proyecto de <strong class="strong">pagina web</strong>, esto con la finalidad de mantener clientes contentos en la fecha prevista , porque a nadie le agrada esperar y sabemos que la palabra vale ...</p>
+									<p class="left-align parrafosServiciosCubo">- Es la forma como me tomo cada proyecto de <strong class="strong">pagina web</strong>, esto con la finalidad de mantener clientes contentos en la fecha prevista , porque a nadie le agrada esperar y sabemos que la palabra vale ...</p>
 								</div>
 							</div>
 						</div>
@@ -254,21 +264,21 @@
 							<p class="parrafosServicios">Para desarrollar una aplicacion web, pagina personal, blog, o tienda en linea se debe tener conocimiento y dominio de diferentes <strong>lenguajes de programación</strong> y dependiendo de el grado de complejidad se deben utilizar unos u otros, por eso siempre estoy enfocado en aprender distintas tecnologias y lenguajes para el desarrollo oportuno, esto con la finalidad de brindar un servicio profesional y que el cliente quede feliz y satisfecho con la inversion hecha.</p>
 
 							<h4 class="titulosServicios text-align hide-on-med-and-down">Creación de Tiendas online, Blogs, Portafolios, Landing pages empresariales. </h4>
-							<p class="parrafosServicios hide-on-med-and-down">Si estas leyendo esta sección es porque :<br> - Opcion (1) estas interesado en darte a conocer por alguna actividad que realizes y quieras ser reconocido/a por lo que haces y deseas tener un <strong>blog</strong> donde escribir tus ideas, notas, recetas y lo sepa la gente.<br> - Opcion (2) Eres emprendedor y quieres comerte al mundo ofreciendo productos unicos y asi comenzar a tener presencia online que se traduciran en mas ventas y trafico para tu negocio.<br> - Opcion (3) Tienes una <strong>empresa</strong> y necesitas una <strong>pagina web</strong> para tener mas contacto y cercania con tus clientes o tener mas control y llevar registro de lo que ingresa y lo que no a la misma ademas de tener mas presencia y renombre ante tu competencia.<br>- Opcion (4) Eres estudiante universitario y necesitas soporte para entregar el proyecto de alguna <strong>pagina web</strong> en los lapsos estipulados por tus asesores o profesores.<br><p class="parrafosServicios"> Cualquiera que sea tu opcion estas en el lugar indicado contacta mis servicios de <strong>freelance</strong> para hacer un estudio de la <strong>pagina o sitio web</strong> que deseas y asi puedas obtener lo que necesitas...</p></p>
+							<p class="parrafosServicios hide-on-med-and-down">Si estas leyendo esta sección es porque :<br> - Opcion (1) estas interesado en darte a conocer por alguna actividad que realices y quieras ser reconocido/a por lo que haces y deseas tener un <strong>blog</strong> donde escribir tus ideas, notas, recetas y lo sepa la gente.<br> - Opcion (2) Eres emprendedor y quieres comerte al mundo ofreciendo productos unicos y asi comenzar a tener presencia online que se traducirian en mas ventas y trafico para tu negocio.<br> - Opcion (3) Tienes una <strong>empresa</strong> y necesitas una <strong>pagina web</strong> para tener mas contacto y cercania con tus clientes o tener mas control y llevar registro de lo que ingresa y lo que no a la misma.<br>- Opcion (4) Eres estudiante universitario y necesitas soporte para entregar el proyecto ya finiquitado en los lapsos estipulados por tus asesores o profesores.<br><p class="parrafosServicios"> Cualquiera que sea tu opcion estas en el lugar indicado contacta mis servicios de <strong>freelance</strong> para hacer un estudio de tu problematica y asi brindarte la solución que mas se adapte a tus necesidades.</p></p>
 
 
 
 							<h4 class="titulosServicios text-align">Optimizacion de tu pagina web, blog, tienda online (SEO + VENTAS)</h4>
-							<p class="parrafosServicios">Si tienes en mente abrir una <strong>tienda online</strong> o hacerte viral en google que te busquen y te encuentren facil o aparezcas sin buscarte, debes tener presente que hoy en dia existe mucha gente que te pueden diseñar y hacer una <strong>pagina web</strong> sin muchos conocimientos del tema, pero no de una manera profesional, Optimizarla en <strong>SEO</strong> ya es otro nivel. Alcanzar un buen posicionamiento en resultados de búsqueda lleva su tiempo y dedicacion.
-								tambien debes tener claro lo que es el <strong>SEO</strong> esto es nada mas y nada menos como <strong>google</strong> y otros buscadores te integran o(indexan) en su buscador, teniendo google el 98% de todas las busquedas diarias a nivel mundial. de manera que para que google te lea y te indexe en buenas posiciones se debe llevar una metodologia, que no se hace de un dia para otro es un conjunto de pasos y tecnicas bien estudiadas que permitiran al <strong>freelance</strong> tratar de posicionar la <strong>pagina web</strong> trabajada entre las primeras busquedas, esto hace la diferencia entre una buena y trabajada<strong> pagina web</strong> a cualquier cosa que te venden como bueno por alli con puro texto html sin mucho sentido.</p><br> <p class="parrafosServicios">Como trabajador <strong>freelance</strong> me dedico a usar distintos metodos para llevar a cabo esta acción, debes saber que de nada sirve el <strong>sitio web</strong> mas hermoso y pintoresco del mundo, si no estas en google, ya que no vas a aparecer y esto de que nos serviria?con propiedad te lo digo, DE NADA!<br><br><p class="parrafosServicios"> De esta forma el futuro dueño de la <strong>pagina web</strong> va a sentirse muy satisfecho ya que obtendra grandes beneficios a favor de su marca o negocio : mas visitas, mas ventas, mas reconocimiento, mas interacción y podras llegar a estar encima de tu competencia dependiendo del posicionamiento que estos tengan en la www.</p></p>
+							<p class="parrafosServicios">Si tienes en mente abrir una <strong>tienda online</strong> o que en google te busquen y te encuentren de una manera mas facil o aparezcas sin buscarte :D, debes tener presente que hoy en dia existe mucha gente que te pueden diseñar y hacer una <strong>pagina web</strong> sin muchos conocimientos del tema, pero no de una manera profesional, Optimizarla en <strong>SEO</strong> ya es otro nivel. Alcanzar un buen posicionamiento en resultados de búsqueda lleva su tiempo y dedicacion.
+								tambien debes tener claro lo que es el <strong>SEO</strong> esto es nada mas y nada menos la forma en como <strong>google</strong> y otros buscadores te integran o(indexan) en su buscador, teniendo google el 98% de todas las busquedas diarias a nivel mundial. de manera que para que google te lea y te indexe en buenas posiciones se debe llevar una metodologia, que no se hace de un dia para otro es un conjunto de pasos y tecnicas bien analizadas que permitiran al <strong>freelance</strong> tratar de posicionar la <strong>pagina web</strong> entre las primeras busquedas dependiendo el nicho de mercado al cual te vas a dirigir, esto hace la diferencia entre una buena y bien trabajada<strong> pagina web</strong> a cualquier cosa que te vendan como bueno por alli con texto html sin mucho sentido.</p><br> <p class="parrafosServicios">Como trabajador <strong>freelance</strong> me dedico a usar distintos metodos para llevar a cabo esta acción, Porque de nada sirve el <strong>sitio web</strong> mas hermoso y pintoresco del mundo, si no estas posicionado en google, ya que no vas a aparecer y esto a la final no valdria de mucho.<br><br><p class="parrafosServicios"> Trabajando de este modo el futuro dueño de la <strong>pagina web</strong> va a sentirse muy satisfecho ya que obtendria grandes beneficios a favor de su marca o negocio : mas visitas, mas ventas, mas reconocimiento, mas interacción y podras llegar a estar encima de tu competencia dependiendo del posicionamiento que estos tengan.</p></p>
 
 								<h4 class="titulosServicios text-align">Enfoque a la usabilidad y rapidez (de carga)</h4>
-								<p class="parrafosServicios">El cliente al invertir en su <strong>sitio web</strong> bien sea para darse a conocer, abrir un negocio online, escribir en un blog para compartir ideas, recetas o trucos. Antes de que cierre el negocio con el <strong>freelance</strong> (Mi persona) primero debe asegurarse bien sea por pruebas o test de <strong>velocidad</strong>, que el producto final cumpla con algunos requisitos como lo son la <strong>usabilidad</strong> y la rapidez de carga...</p>	
+								<p class="parrafosServicios">El cliente al invertir en su <strong>sitio web</strong> bien sea para darse a conocer, abrir un negocio online, escribir en un blog para compartir ideas, recetas o trucos. Antes de que cierre el negocio con el <strong>freelance</strong> (Mi persona) primero debe asegurarse bien sea mediante pruebas o test de <strong>velocidad</strong>, que el producto final cumpla con algunos requisitos como lo son la <strong>usabilidad</strong> y la rapidez de carga...</p>	
 								<p class="parrafosServicios">
-									Estamos en el 2017 y las personas en el mundo se conectan mas al internet desde dispositivos moviles que desde computadoras de escritorio, por esta razon y muchas mas veo necesario siempre realizar la <strong>pagina web </strong>con diseño web adaptativo, que no es mas que la construcción de webs adaptables a cualquier tamaño de pantalla tanto de computadoras de escritorio como de tablets y móviles, con las que se ofrece una experiencia de navegación cómoda y atractiva para el usuario,y personalizada para cada dispositivo.<br><br>
+									Estamos en el 2017 y las personas en el mundo se conectan mas al internet desde dispositivos moviles que desde computadoras de escritorio, por esta razon y muchas mas veo necesario siempre realizar la <strong>pagina web </strong>con diseño web adaptativo, que no es mas que la construcción de webs adaptables a cualquier tamaño de pantallas, tanto de computadoras de escritorio como de tablets y móviles, con lo que se busca una experiencia en navegación cómoda y atractiva para el usuario final, y ademas personalizada para cada dispositivo.<br><br>
 								</p>
 								<p class="parrafosServicios">
-									Estudios como el reciente de www.pagescoring.com (2017) han comprobado que al menos el 50% de los visitantes esperan que una web, cargue en 3 segundos o menos, y si no carga en este tiempo las personas o futuros clientes se van, Y que el 40% de los visitantes abandonan el sitio si toman mas de 4 segundos en cargar, esto no es lo que queremos y por eso trabajo para ello, queremos que los </strong>usuarios y clientes</strong> regresen porque les agrade su velocidad y la forma facil de acceder al contenido de una manera bien intuitiva gracias a el diseño y a la manera en como se va mostrando toda la información mas importante de todo el <strong>sitio web</strong>, pues de esto trata la usabilidad, por eso como <strong>freelance</strong> pongo especial enfasis en poner en practica todos estos conceptos tan fundamentales antes de hacer el lanzamiento(deploy) de nuestra aplicación o <strong>pagina web</strong>  a la nube.</p>
+									Estudios como el reciente de www.pagescoring.com (2017) han comprobado que al menos el 50% de los visitantes esperan que una web, cargue en 3 segundos o menos, y si no carga en este tiempo las personas o futuros clientes se van, Y que el 40% de los visitantes abandonan el sitio si toman mas de 4 segundos en cargar, esto no es lo que queremos y por eso trabajo para ello, queremos que los </strong>usuarios y clientes finales</strong> regresen porque les agrade su velocidad y la forma facil de acceder al contenido de una manera bien intuitiva, gracias a el diseño y a la manera en como se va mostrando la información mas importante de todo el <strong>sitio web</strong>, pues de esto trata la usabilidad, por eso como <strong>freelance</strong> pongo especial enfasis en poner en practica todos estos conceptos tan fundamentales antes de hacer el lanzamiento(deploy) de nuestra aplicación o <strong>pagina web</strong></p>
 								</div>
 							</div>
 
@@ -293,7 +303,7 @@
 										<img class="activator" src="img/proyecto-portafolio.png">
 									</div>
 									<div class="card-content">
-										<span class="card-title  grey-text text-darken-4"><a target="_blank" style="color:#F4511E;" href="//localhost/portafolio/portafolio/">Portafolio web</a><i class="material-icons right">desktop_windows</i></span>
+										<span class="card-title  grey-text text-darken-4"><a target="_blank" style="color:#F4511E;" href="http://roberto-figuera.16mb.com">Portafolio web</a><i class="material-icons right">desktop_windows</i></span>
 									</div>
 									<div class="card-reveal">
 										<span style="font-weight: bold;" class="card-title black-text text-darken-4">Descripción de el portafolio web <i class="material-icons right">close</i></span>
@@ -536,7 +546,7 @@
 						<div style="border-top: 2px solid white;" class="footer-copyright page-footer purple darken-5 ">
 							<div class="container">
 								© 2017 Copyright 
-								<address class="right"> Designed and programmed by : <a href="#">Roberto Figuera</a> | <i class="tiny material-icons">face</i></address>
+								<address class="right"> Designed and programmed by : <a target="_blank" style="color:black" href="https://www.facebook.com/RobertoDevel">Roberto Figuera</a> | <i class="tiny material-icons">face</i></address>
 							</div>
 						</div>
 					</footer>
@@ -544,8 +554,7 @@
 				<!-- endfooter -->
 			</section>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-			<script src="js/materialize.js"></script>
-			<script src="js/parallax.js"></script>
+			<script src="js/materialize.min.js"></script>
 			<script src="js/animatescroll.min.js"></script>
 			<script src="js/misScripts.js"></script>
 			<script>			
